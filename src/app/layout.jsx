7971b90a3c7c0cca7356f50d5a9876f62c698b8c
TezzaMan <terry-ochodnicky@hotmail.co.uk>
2023-02-25 13:38:00 +0000

    Created navbar component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import Providers from './providers'
 
@@ -11,6 +12,8 @@ export default function RootLayout({ children }) {
           {/* Header */}
         <Header/>
         {/* Navbar */}
+
+        <Navbar />
 
         {/* SearchBox */}
         
